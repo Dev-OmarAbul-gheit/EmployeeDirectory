908f3532118818fc8349d625b4985ce8c7971fed
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-01-09 10:05:58 +0200

    Customized the list page of <Department> model in the admin panel.

--- EmployeeDirectory/urls.py
+++ EmployeeDirectory/urls.py
@@ -19,10 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-admin.site.site_header = 'Employee Directory Admin'
-admin.site.index_title = 'Admin'
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls))
--- directory/admin.py
+++ directory/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin, messages
+from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html, urlencode 
 from .models import Employee, Department
-
 
 admin.site.site_header = 'Employee Directory Admin'
 admin.site.index_title = 'Admin'
-
 
 class SalaryFilter(admin.SimpleListFilter):
     title = 'salary'
@@ -57,5 +58,65 @@ class EmployeeAdmin(admin.ModelAdmin):
         self.message_user(
             request,
             f"{employees_count} employees have got a salary deducte by 10%",
+            messages.WARNING   
+        )
+
+
+class EmployeesCountFilter(admin.SimpleListFilter):
+    title = 'team size'
+    parameter_name = 'employees_count'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('small', 'Small Team'),
+            ('big', 'Big Team'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'small': 
+            return queryset.filter(employees_count__lt = 5)
+        elif self.value() == 'big':
+            return queryset.filter(employees_count__gt = 5)
+
+
+class EmployeeInline(admin.TabularInline):
+    model = Employee
+    extra = 0
+    min_num = 1
+
+@admin.register(Department)
+class DepartmentModel(admin.ModelAdmin):
+    inlines = [EmployeeInline]
+    list_display = ['id', 'name', 'employees_count']
+    list_editable = ['name']
+    list_per_page = 10
+    search_fields = ['name__istartswith']
+    list_filter = ['name', EmployeesCountFilter]
+    actions = ['remove_employees']
+
+    @admin.display(ordering='employees_count')
+    def employees_count(self, department):
+        url = reverse('admin:directory_employee_changelist') \
+              + '?' \
+              + urlencode({'department__id': str(department.id)})
+
+        return format_html(f'<a href="{url}">{department.employees_count}</a>')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            employees_count = Count('employees')
+        )
+    
+    @admin.action(description= 'Remove all employees')
+    def remove_employees(self, request, queryset):
+        employees_count = 0
+        departments = [department.name for department in list(queryset)]
+        for department in queryset:
+            employees_count += department.employees.count()
+            department.employees.update(department_id = None)
+        
+        self.message_user(
+            request,
+            f"{employees_count} employees have removed from {departments} departments.",
             messages.WARNING   
         )
